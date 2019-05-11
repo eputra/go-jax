747bac38aa93e9f90445ea588333c80cc84124e5
Eka Putra <ekaputtra@gmail.com>
2019-05-11 16:18:13 +0700

    download file

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ import (
 	"path/filepath"
 )
 
+type M map[string]interface{}
+
 func main() {
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/save", handleSave)
 	http.HandleFunc("/upload", handleUpload)
+	http.HandleFunc("/list-files", handleListFiles)
+	http.HandleFunc("/download", handleDownloadView)
+	http.HandleFunc("/download/get", handleDownloadGet)
 
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
@@ -97,6 +102,68 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.Write([]byte("all files uploaded"))
+		return
+	}
+}
+
+func handleDownloadView(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("download.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleListFiles(w http.ResponseWriter, r *http.Request) {
+	files := []M{}
+	basePath, _ := os.Getwd()
+	filesLocation := filepath.Join(basePath, "files")
+
+	err := filepath.Walk(filesLocation, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		files = append(files, M{"filename": info.Name(), "path": path})
+		return nil
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(files)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func handleDownloadGet(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	path := r.FormValue("path")
+	f, err := os.Open(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	contentDisposition := fmt.Sprintf("attachment; filename=%s", f.Name())
+	w.Header().Set("Content-Disposition", contentDisposition)
+
+	if _, err := io.Copy(w, f); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
